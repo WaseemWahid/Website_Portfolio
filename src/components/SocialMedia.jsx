cb474f92fd1df11b5e090b5810ed6e4cdf581657
Waseem <wahid.waseem@gmail.com>
2022-06-02 07:56:28 -0700

    added Github link

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs'
 import { FaFacebookF } from 'react-icons/fa'
+import { ImGithub } from 'react-icons/im'
 
 
 const SocialMedia = () => {
@@ -19,6 +20,11 @@ const SocialMedia = () => {
             <div>
                 <a href="https://www.instagram.com/_waseem_wahid_/" target="_blank" rel="noreferrer">
                     <BsInstagram/>
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/WaseemWahid" target="_blank" rel="noreferrer">
+                    <ImGithub/>
                 </a>
             </div>
         </div>
